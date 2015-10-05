e362939d891ceb34fd60e33d327b6c1bd4ac90bf
Niji <niji@qburst.com>
2015-10-05 14:57:05 +0530

    api-responsive

--- api/js/controller/detailsCntrl.js
+++ api/js/controller/detailsCntrl.js
@@ -5,14 +5,14 @@ app.controller('detailsCntrl', ["myService", '$scope', '$location', function (my
      
     $scope.companyName = myService.data;
     
-        $scope.name = $scope.companyName[$scope.current].details.company;
-        $scope.head = "COMPANY PROFILE";
-        $scope.id = $scope.companyName[$scope.current].id;
-        $scope.title = $scope.companyName[$scope.current].title;
-        $scope.cost =  $scope.companyName[$scope.current].cost;
-        $scope.stockleft = $scope.companyName[$scope.current].details.stockLeft;
-        $scope.buylimit = $scope.companyName[$scope.current].details.buyLimit;
-        $scope.summary = $scope.companyName[$scope.current].details.summery;
+    $scope.name = $scope.companyName[$scope.current].details.company;
+    $scope.head = "COMPANY PROFILE";
+    $scope.id = $scope.companyName[$scope.current].id;
+    $scope.title = $scope.companyName[$scope.current].title;
+    $scope.cost =  $scope.companyName[$scope.current].cost;
+    $scope.stockleft = $scope.companyName[$scope.current].details.stockLeft;
+    $scope.buylimit = $scope.companyName[$scope.current].details.buyLimit;
+    $scope.summary = $scope.companyName[$scope.current].details.summery;
     
 }]);
 
--- api/js/controller/mainCntrl.js
+++ api/js/controller/mainCntrl.js
@@ -1,12 +1,15 @@
-app.controller('mainCntrl', ["myService", '$scope', '$location', function (myService, $scope, $location) {
+app.controller('mainCntrl', ["myService", '$scope', '$location', '$timeout', function (myService, $scope, $location, $timeout) {
 
     "use strict";
-    
+    $scope.detail = [];
     myService.details();
     console.log(myService);
-    $scope.data = myService.data;
+    $scope.data = myService;
     
-    
+    $scope.detail = $scope.data.data;
+    //$timeout( function(){ console.log("in" + $scope.detail); }, 5000);
+
+    console.log($scope.detail);
     $scope.go = function (path, index) {
         $location.path(path);
         myService.itemClicked = index;
